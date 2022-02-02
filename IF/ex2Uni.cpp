5c1cf93eb96e86cfabc35f19483b2c153acb40bc
Muhammad Abdullah <mafivs51@gmail.com>
2022-02-03 00:58:19 +0500

    if

---
+++ IF/ex2Uni.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+char val;
+cout << "Are your friends going to university ? (Press Y for Yes and N for No)...";
+cin>> val;
+if (val=='Y'){
+cout<<"Yes I am Going To University";
+} 
+}
---
+++ IF/ex3Welcome.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+string name;
+cout << "Please Enter Your Name...";
+cin>> name;
+if (name=="Ali"){
+cout<<"Welcome to Uet, Ali";
+} 
+}
